215fe60e202a080036c93d414aa2eb6d5187445b
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2021-10-04 13:16:19 +0200

    Cambio de colores

--- Holamundo_04oct.java
+++ Holamundo_04oct.java
@@ -1,9 +1,5 @@
 public class HolaMundo{
 	public static void main(String[] args){
 		System.out.println("Modificado");
-		
-		String naranja = "\033[33m";
-		
-		System.out.println("Hola mundo");
 	}
 }
